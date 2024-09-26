bb34bb94d895350869af7e4ad78117d749c17839
yyoshidaweb <82887464+yyoshidaweb@users.noreply.github.com>
2024-09-26 17:53:13 +0900

    first commit

---
+++ src/main/java/jp/eightbit/exam/OnlineWorkbookApplication.java
@@ -0,0 +1,13 @@
+package jp.eightbit.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWorkbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineWorkbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/eightbit/exam/OnlineWorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.eightbit.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWorkbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
